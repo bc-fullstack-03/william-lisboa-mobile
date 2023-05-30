9c56c23e0d21c27c56b501552fa4ebf11806a37c
will763 <killzasper@gmail.com>
2023-05-30 01:56:13 -0300

    feat: add friends component

---
+++ src/Screens/Friends/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function Friends() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Friends Screen</Text>
+      </View>
+    );
+}
+
+export default Friends
