afa15f9a32342ebd3ee60c89f65f75e9bb3482e5
will763 <killzasper@gmail.com>
2023-05-30 01:52:03 -0300

    style: add app theme

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+    COLORS: {
+        BACKGROUND_900: "#121214",
+
+        CAPTION_400: "#7C7C8A",
+        TEXT: "#EEEEEE",
+    },
+    FONT_FAMILY: {
+        REGULAR: "Inter_400Regular",
+        SEMI_BOLD: "Inter_600SemiBold",
+        BOLD: "Inter_700Bold",
+        BLACK: "Inter_900Black"
+      },
+    FONT_SIZE: {
+        SM: 14,
+        MD: 16,
+        LG: 24,
+      },
+  };
