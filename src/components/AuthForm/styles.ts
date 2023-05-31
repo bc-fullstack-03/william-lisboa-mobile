53c9bca9d144618943ae4c244a6d3f6774991787
will763 <killzasper@gmail.com>
2023-05-31 18:44:06 -0300

    feat: add style of AuthForm component

---
+++ src/components/AuthForm/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 48,
+  },
+  containerPosition: {
+    alignItems: 'center',
+    marginTop: 0,
+    paddingBottom: 24,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
