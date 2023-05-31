8716fdbbc8fb107cbbc36642e6f9aea4d5dbbf59
will763 <killzasper@gmail.com>
2023-05-31 18:38:25 -0300

    feat: add new properties

--- src/THEME/index.ts
+++ src/THEME/index.ts
@@ -1,9 +1,14 @@
 export default {
     COLORS: {
-        BACKGROUND_900: "#121214",
+      BACKGROUND_900: "#121214",
+      BACKGROUND_600: '#202024',
 
-        CAPTION_400: "#7C7C8A",
-        TEXT: "#EEEEEE",
+      INPUT: '#7C7C8A',
+
+      CAPTION_400: "#7C7C8A",
+      TEXT: "#EEEEEE",
+
+      BUTTON: '#81D8F7',
     },
     FONT_FAMILY: {
         REGULAR: "Inter_400Regular",
