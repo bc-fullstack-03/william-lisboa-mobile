4a04c3fce68f7634e10b0652c1a6399bba9dac33
will763 <killzasper@gmail.com>
2023-05-31 18:40:54 -0300

    feat: add style of Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+import THEME from '../../THEME';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    maxWidth: '100%',
+    minWidth: 240,
+    padding: 12,
+    borderRadius: 12,
+    backgroundColor: THEME.COLORS.BACKGROUND_600,
+  },
+  input: {
+    marginStart: 12,
+    flex: 1,
+    color: THEME.COLORS.INPUT,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+  },
+});
