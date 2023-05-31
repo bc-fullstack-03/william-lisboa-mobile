b5986e24cbd4b576862722359e77411b8103a8eb
will763 <killzasper@gmail.com>
2023-05-31 18:48:08 -0300

    feat: add style of SignUp component

---
+++ src/Screens/SignUp/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+import THEME from "../../THEME"
+
+export const styles = StyleSheet.create({
+    link: {
+        color: THEME.COLORS.CAPTION_400,
+        fontSize: THEME.FONT_SIZE.SM,
+        textAlign: 'center',
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        textDecorationLine: 'underline',
+      },
+})
