9c49cfdad577bcbfed599dc2b65b0b889c201cd0
will763 <killzasper@gmail.com>
2023-06-03 00:27:57 -0300

    feat: handle input user to register user

--- src/Screens/SignUp/index.tsx
+++ src/Screens/SignUp/index.tsx
@@ -1,19 +1,30 @@
 import { Text, TouchableOpacity } from 'react-native';
 import AuthForm from '../../components/AuthForm';
 import { styles } from './styles';
+import { Context as AuthContext } from '../../context/AuthContext';
+import { useContext } from 'react';
+import Spacer from '../../components/Spacer';
 
 function SignUp({navigation}) {
+  const { register, errorMessage } = useContext(AuthContext)
+  
   return (
     <>
       <AuthForm
         authFormSubtitle='Registre-se agora mesmo!'
         submitFormButtonText='Cadastrar'
+        submitFormButtonAction={register}
       />
       <TouchableOpacity 
         onPress={()=> { navigation.navigate("Login")}}
       >
         <Text style={styles.link}>Já possui conta? Entre agora!</Text>
       </TouchableOpacity>
+      {errorMessage && (
+        <Spacer>
+          <Text style={styles.error}>{errorMessage}</Text>
+        </Spacer>
+      )}
     </>
   );
 }
