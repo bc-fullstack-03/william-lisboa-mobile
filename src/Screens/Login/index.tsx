a451fa2b0c0ffad08cffed398b53f22da0637a2e
will763 <killzasper@gmail.com>
2023-06-03 00:27:36 -0300

    feat: handle input user to login user

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,20 +1,30 @@
 import { TouchableOpacity, Text } from 'react-native';
+import { Context as AuthContext } from '../../context/AuthContext';
 
 import { styles } from './styles';
 import AuthForm from '../../components/AuthForm';
+import { useContext } from 'react';
+import Spacer from '../../components/Spacer';
 
 function Login({navigation}) {
+  const { login, errorMessage } = useContext(AuthContext)
     return (
       <>
         <AuthForm 
           authFormSubtitle='Faça login e começe a usar!'
           submitFormButtonText='Entrar'
+          submitFormButtonAction={login}
         />
         <TouchableOpacity 
           onPress={()=> { navigation.navigate("SignUp")}}
         >
           <Text style={styles.link}>Não possui conta? Crie uma agora!</Text>
         </TouchableOpacity>
+        {errorMessage && (
+         <Spacer>
+           <Text style={styles.error}>{errorMessage}</Text>
+         </Spacer>
+        )}
         </>
     );
 }
