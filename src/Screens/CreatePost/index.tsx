e15cf1cf2d9d2d928b98dfd3b3f86c75cdf62c2d
will763 <killzasper@gmail.com>
2023-06-05 02:43:22 -0300

    feat: add createPost screen

---
+++ src/Screens/CreatePost/index.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import { Text } from "react-native-svg";
+
+export function CreatePost(){
+    return (
+        <View><Text>olahhh</Text></View>
+    )
+    
+}
