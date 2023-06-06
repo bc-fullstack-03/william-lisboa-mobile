c814db5f78af573a9cee2766b73c6a7877f12f8e
will763 <killzasper@gmail.com>
2023-06-06 05:05:45 -0300

    feat: add style of create post screen

---
+++ src/Screens/CreatePost/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import THEME from "../../THEME";
+
+export const styles = StyleSheet.create({
+    container: {    
+        flex: 1,
+        paddingTop: 12,
+    },
+    heading: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 12,
+        paddingBottom: 12,
+        borderBottomColor: THEME.COLORS.BORDER,
+        borderBottomWidth: 1,
+    },
+    userNameText: {
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        fontSize: THEME.FONT_SIZE.MD,
+        color: THEME.COLORS.TEXT,
+        marginStart: 12,
+    },
+});
