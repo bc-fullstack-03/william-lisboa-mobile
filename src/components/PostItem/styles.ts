4c19917d42b52135255bfde5cb75f9adbe750fe0
will763 <killzasper@gmail.com>
2023-06-06 01:14:11 -0300

    feat: add style of postItem component

---
+++ src/components/PostItem/styles.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from "react-native";
+import THEME from "../../THEME";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        borderBottomColor: THEME.COLORS.BORDER,
+        borderBottomWidth: 1,
+        paddingBottom: 12,
+    },
+    heading: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 12,
+    },
+    profileName: {
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        fontSize: THEME.FONT_SIZE.MD,
+        color: THEME.COLORS.TEXT,
+        marginStart: 12,
+    },
+    postTitle: {
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        fontSize: THEME.FONT_SIZE.MD,
+        color: THEME.COLORS.TEXT,
+        marginStart: 12,
+    },
+    image: {
+        resizeMode: "contain",
+        height: 160,
+        borderRadius: 12,
+    },
+    description: {
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        fontSize: THEME.FONT_SIZE.SM,
+        color: THEME.COLORS.TEXT,
+        marginStart: 12,
+    },
+    footer: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 24,
+    },
+    number: {
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        fontSize: THEME.FONT_SIZE.SM,
+        color: THEME.COLORS.TEXT,
+        marginStart: 4,
+        marginEnd: 24,
+    },
+});
