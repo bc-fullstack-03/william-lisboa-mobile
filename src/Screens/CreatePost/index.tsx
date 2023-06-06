de1c215067ff489c61962b06a03a76fe366bb39e
will763 <killzasper@gmail.com>
2023-06-06 05:05:25 -0300

    feat: implement create post screen

--- src/Screens/CreatePost/index.tsx
+++ src/Screens/CreatePost/index.tsx
@@ -1,9 +1,60 @@
-import { View } from "react-native";
-import { Text } from "react-native-svg";
+import React, { useContext, useState } from "react";
+import { View, Text } from "react-native";
+import { Context as AuthContext } from "../../context/AuthContext";
+import { Context as PostContext } from "../../context/PostContext";
 
-export function CreatePost(){
+import { UserCircle } from "phosphor-react-native";
+import { File } from "../../Model/File"
+
+import PostImagePicker from "../../components/PostImagePicker";
+import Spacer from "../../components/Spacer";
+import Button from "../../components/Button";
+import { styles } from "./styles";
+import { Input } from "../../components/Input";
+import THEME from "../../THEME";
+
+export function CreatePost() {
+    const { user } = useContext(AuthContext);
+    const { createPost } = useContext(PostContext);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState<File>();
+   
     return (
-        <View><Text>olahhh</Text></View>
-    )
-    
+        <View  style={styles.container}>
+            <View style={styles.heading}>
+                <UserCircle color="white" size={48} weight="thin" />
+                <Text style={styles.userNameText}>{user}</Text>
+            </View>
+            <Spacer />
+            <Input.Root>
+                <Input.Input 
+                    value={title}
+                    onChangeText={setTitle}
+                    placeholder='Qual é o título do post?'
+                    placeholderTextColor={THEME.COLORS.INPUT}
+                    autoCorrect={false}
+                />
+            </Input.Root>
+            <Spacer />
+            <Input.Root>
+                <Input.Input 
+                    value={description}
+                    onChangeText={setDescription}
+                    placeholder='Qual é a descrição do post?'
+                    placeholderTextColor={THEME.COLORS.INPUT}
+                    autoCorrect={false}
+                />
+            </Input.Root>
+            <Spacer />
+            <PostImagePicker onFileLoaded={setImage} />
+            <Spacer />
+            <Button
+                title="Postar"
+                onPress={() => {
+                    createPost({ title, description, image })
+                }} 
+            />
+        </View>
+    );
 }
