56bf4553c2a339b5695323a1447bf79e83ac010e
will763 <killzasper@gmail.com>
2023-06-06 01:20:04 -0300

    feat: wrapper home with Post Provide

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider as PostProvider } from "../../context/PostContext";
 import { PostList } from "../PostList/index";
 import { CreatePost } from "../CreatePost/index";
 
@@ -6,15 +7,17 @@ const Stack = createNativeStackNavigator();
 
 function Home() {
     return (
-      <Stack.Navigator 
-        screenOptions={{
-          headerShown: false,
-          statusBarStyle: "dark",
-        }}
-      >
-        <Stack.Screen name="PostList" component={PostList} />
-        <Stack.Screen name="CreatePost" component={CreatePost} />
-      </Stack.Navigator>
+      <PostProvider>
+        <Stack.Navigator 
+          screenOptions={{
+            headerShown: false,
+            statusBarStyle: "dark",
+          }}
+        >
+          <Stack.Screen name="PostList" component={PostList} />
+          <Stack.Screen name="CreatePost" component={CreatePost} />
+        </Stack.Navigator>
+      </PostProvider>
     );
 }
 
