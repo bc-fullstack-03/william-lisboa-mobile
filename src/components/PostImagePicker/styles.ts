638612d35ad3c73adcba4031f0e26ef862160bda
will763 <killzasper@gmail.com>
2023-06-06 05:16:26 -0300

    feat: add style of PostImagePicker component

---
+++ src/components/PostImagePicker/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {    
+        justifyContent: "center",
+    },
+    image: {
+        marginTop: 16,
+        alignSelf: "center",
+        width: 200,
+        height: 200,
+        borderRadius: 12,
+    }
+
+});
